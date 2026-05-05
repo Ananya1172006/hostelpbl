003c6936d1d4464e4db9f26cdc8d92977734a6e7
Ananya1172006 <ananyaohol@gmail.com>
2026-05-05 22:01:59 +0530

    edit2

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,6 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your_fallback_secret';
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
-});
 
 const db = new sqlite3.Database(path.join(__dirname, 'database.db'));
 
